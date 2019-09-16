0118629272eeb1fd006c9a091378f9e5dfec772b
GSCWJ <jizhugaoyongyuan@qq.com>
2019-09-16 18:04:36 +0800

    2019.09.12-1: 1、放假备份

---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <el-row :gutter="20">
+        <el-col :span="12" :offset="6"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+</template>
+
+<script>
+    import loginForm from '../components/loginForm'
+    export default {
+        name: "Login",
+        components: {
+            loginForm
+        },
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .grid-content {
+        border-radius: 4px;
+        min-height: 36em;
+    }
+</style>
